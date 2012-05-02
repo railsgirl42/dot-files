4f3bafc582887ac7770b21f70849b0c1ae5bf2ec
railsgirl42 <railsgirl42@rocketmail.com>
2012-05-02 15:34:35 +0100

    updated repository name

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -1,7 +1,7 @@
 !/bin/bash
 
 cd &&
-[ -d '.dot-files' ] || git clone git://github.com/traceym/dot-files.git .dot-files &&
+[ -d '.dot-files' ] || git clone git://github.com/railsgirl42/dot-files.git .dot-files &&
 ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
   [ "$f" == '.dot-files/files/.' ] ||
   [ "$f" == '.dot-files/files/..' ] ||
